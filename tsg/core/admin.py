4f74eabd45d2cf7025333c099a5a26d1a47659cf
Germanv33 <71227097+Germanv33@users.noreply.github.com>
2021-11-07 19:03:07 +0300

    Delete tsg directory

--- tsg/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core.models import Articles, Documents, User
-
-
-# Register your models here.
-admin.site.register(Articles)
-admin.site.register(Documents)
-admin.site.register(User)
-
